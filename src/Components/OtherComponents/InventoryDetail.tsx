7b20609122eb03f2c055ddbd5583477559fbc2ac
Ayesha Shamim <ayeshashamim.011@gmail.com>
2025-08-07 16:28:42 +0500

    Enhance InventoryDetail with new display logic

--- src/Components/OtherComponents/InventoryDetail.tsx
+++ src/Components/OtherComponents/InventoryDetail.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const InventoryDetail: React.FC = () => {
 
     const orderDetails = {
-        orderId: '47895303',
-        orderDate: '47895303',
+        orderId: '123456789',
+        orderDate: '',
         totalPrice: 'PKR 30,000',
         status: 'Shipped',
         shippingAddress: 'Sybrid Private Limited, street 14, I-9/2, Islamabad, Pakistan',
@@ -26,93 +26,158 @@ const InventoryDetail: React.FC = () => {
                 quantity: '1 bag',
                 price: '15,000 PKR',
                 image: '/images/urea2.svg'
+            },
+            {
+                id: 3,
+                name: 'Urea',
+                category: 'Fertilizer',
+                quantity: '1 bag',
+                price: '15,000 PKR',
+                image: '/images/urea2.svg'
             }
         ]
     };
 
     return (
-        <div className="mt-0 mb-0 sm:p-6 md:p-8 min-h-screen">
-            
+        <div className="mt-0 mb-0 h-screen">
+
             {/* <h1 className="text-center text-[24px] text-Black font-Montserrat font-bold mb-8">Inventory</h1> */}
 
-
+            {/* order details */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-4 gap-x-6 mb-4 text-[14px] font-Montserrat">
 
                 <div className="flex gap-2 ">
-                    <span className="text-Gray font-normal">Order ID</span>
-                    <span className="text-Black font-semibold">{orderDetails.orderId}</span>
+
+                    <span className="text-Gray font-normal">
+                        Order ID
+                    </span>
+
+                    <span className="text-Black font-semibold">
+                        {orderDetails.orderId}
+                    </span>
+
                 </div>
 
+
                 <div className="flex gap-2">
-                    <span className="text-Gray font-normal">Order Date</span>
+
+                    <span className="text-Gray font-normal">
+                        Order Date
+                    </span>
+
                     <span className="text-Black font-semibold">
+
                         {new Date().toLocaleDateString('en-US', {
                             year: 'numeric',
                             month: 'long',
                             day: 'numeric',
                         })}
+
                     </span>
                 </div>
 
                 <div className="flex gap-2">
-                    <span className="text-Gray font-normal">Total Price</span>
-                    <span className="text-Black font-semibold">{orderDetails.totalPrice}</span>
+
+                    <span className="text-Gray font-normal">
+                        Total Price
+                    </span>
+
+                    <span className="text-Black font-semibold">
+                        {orderDetails.totalPrice}
+                    </span>
+
                 </div>
 
                 <div className="flex gap-2">
-                    <span className="text-Gray font-normal">Status</span>
-                    <span className="text-Golden font-medium font-Poppins">{orderDetails.status}</span>
+
+                    <span className="text-Gray font-normal">
+                        Status
+                    </span>
+
+                    <span className="text-Golden font-medium font-Poppins">
+                        {orderDetails.status}
+                    </span>
+
                 </div>
 
                 <div className="flex gap-2 col-span-1 sm:col-span-2">
-                    <span className="text-Gray font-normal">Shipping Details</span>
-                    <span className="text-Black font-semibold">{orderDetails.shippingAddress}</span>
+
+                    <span className="text-Gray font-normal">
+                        Shipping Details
+                    </span>
+
+                    <span className="text-Black font-semibold">
+                        {orderDetails.shippingAddress}
+                    </span>
+
                 </div>
 
             </div>
 
-
+            {/* product information */}
             <div className="mb-4">
-                <h2 className="text-Golden text-[18px] font-Montserrat font-bold">Product Information</h2>
+                <h2 className="text-Golden text-[18px] font-Montserrat font-bold">
+                    Product Information
+                </h2>
 
                 <p className="text-[14px] font-Montserrat font-normal text-Gray mt-1">
                     Total Products :
-                    <span className="text-[14px] font-Montserrat font-semibold text-Gray">{orderDetails.products.length}</span>
+                    <span className="text-[14px] font-Montserrat font-semibold text-Gray">
+                        {orderDetails.products.length}
+                    </span>
                 </p>
 
             </div>
 
+
+            {/* products */}
             <div className="space-y-1 pb-4">
 
-                {orderDetails.products.map(product => (
+                {orderDetails.products.map((product) => (
 
-                    <div key={product.id} className="flex items-start gap-8 p-2">
+                    <div
+                        className="flex items-start gap-8 px-2 pt-6 pb-7">
 
-                        <img src={product.image} alt={product.name} className="w-[206px] h-[180px] object-contain bg-white rounded shadow-md" />
+                        <div className='w-[206px] h-[180px] flex justify-center items-center bg-white rounded shadow-md'>
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                className="w-[198px] h-[153px] object-contain"
+                            />
+                        </div>
 
-                        <div className='mt-7'>
-                            <h3 className="text-Black text-[24px] font-Montserrat font-semibold">{product.name}</h3>
 
-                            <p className="text-Gray text-[16px] font-Poppins font-normal">
+                        <div className='mt-7 space-y-2'>
 
-                                Category :
+                            <h3 className="text-Black text-[24px] font-Montserrat font-semibold">
+                                {product.name}
+                            </h3>
 
-                                <span className="text-Golden text-[16px] font-Poppins font-normal">{product.category}</span>
+                            <p className="text-Gray text-[16px] font-Poppins font-thin">
+
+                                Category :{" "}
+                                <span className="text-Golden text-[16px] font-Poppins font-normal">
+                                    {product.category}
+                                </span>
+
                             </p>
 
                             <p className="text-Gray text-[16px] font-Poppins font-normal">
 
-                                Quantity:
-
-                                <span className="text-Black text-[16px] font-Poppins font-normal">{product.quantity}</span>
+                                Quantity :{" "}
+                                <span className="text-Black text-[16px] font-Poppins font-normal">
+                                    {product.quantity}
+                                </span>
                             </p>
 
                             <p className="text-Gray text-[16px] font-Poppins font-normal">
 
-                                Price :
-
-                                <span className="text-Black text-[16px] font-Poppins font-normal">{product.price}</span>
+                                Price :{" "}
+                                <span className="text-Black text-[16px] font-Poppins font-normal">
+                                    {product.price}
+                                </span>
                             </p>
+
                         </div>
 
                     </div>
