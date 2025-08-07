4e89d5daf3a3a80eccc4a01e0686316ca228929c
Ayesha Shamim <ayeshashamim.011@gmail.com>
2025-08-07 16:49:22 +0500

    Revise layout structure for better maintainability

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,74 @@
+// import React from "react";
+// import Sidebar from "../CommonComponents/Sidebar";
+// import TopPanel from "../CommonComponents/TopPanel";
+// import { Outlet } from "react-router-dom";
+
+// const SidebarLayout: React.FC = () => {
+//     return (
+
+//         <div className="flex h-screen w-screen overflow-hidden">
+
+//             <Sidebar />
+
+//             <div className="flex-1 flex flex-col overflow-hidden bg-Background_Color">
+
+//                 <TopPanel /> 
+
+//                 <div className="overflow-auto flex-1 px-4 py-4">
+
+//                     <Outlet />
+
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default SidebarLayout;
+
+
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar from "../Components/SideBar/Sidebar";
+import TopPanel from "../Components/TopPanel/TopPanel";
+import { routeConfig } from "../routeConfig";
+
+const Layout = () => {
+
+    const location = useLocation();
+
+    const path = location.pathname;
+
+    const { title, showNotification, showUserInfo, showOrderHistory, showSearch, showSettings, showVerticalBar } = routeConfig[path] || {};
+
+    return (
+        <div className="flex h-screen w-screen overflow-hidden">
+
+            <Sidebar />
+
+            <div className="flex-1 flex flex-col overflow-hidden bg-Background_Color">
+
+                <TopPanel
+
+                    title={title}
+                    showNotification={showNotification}
+                    showUserInfo={showUserInfo}
+                    showOrderHistory={showOrderHistory}
+                    showSearch={showSearch}
+                    showSettings={showSettings}
+                    showVerticalBar={showVerticalBar}
+
+                />
+
+                <div className="p-4 overflow-auto scrollbar-hide">
+
+                    <Outlet />
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
+
